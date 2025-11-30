7f981b6eaf9a7f4d65c4188d905e174e79c51226
Le Gia Bao <10422088@student.vgu.edu.vn>
2025-11-30 09:23:03 +0000

    add person

--- ParkingManagementApp/src/main/java/com/parkingapp/model/Resident.java
+++ ParkingManagementApp/src/main/java/com/parkingapp/model/Resident.java
@@ -1,34 +1,23 @@
 package com.parkingapp.model;
 
-public class Resident {
-    private Long id;
-    private final String name;
+public class Resident extends Person {
     private final String apartment;
-    private String phone;
 
     public Resident() {
-        this.id = null;
-        this.name = null;
+        super();
         this.apartment = null;
-        this.phone = null;
     }
 
     public Resident(Long id, String name, String apartment, String phone) {
-        this.id = id;
-        this.name = name;
+        super(id, name, phone);
         this.apartment = apartment;
-        this.phone = phone;
     }
 
     public Resident(String name, String apartment, String phone) {
         this(null, name, apartment, phone);
     }
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getName() { return name; }
-    public String getApartment() { return apartment; }
-    public String getPhone() { return phone; }
-    public void setPhone(String phone) { this.phone = phone; }
+    public String getApartment() {
+        return apartment;
+    }
 }
